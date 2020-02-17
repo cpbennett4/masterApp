e4784708d551687053926b6303d7a5f829c8cae4
Chucky Bennett <cpbennett4@gmail.com>
2020-02-17 15:44:12 -0500

    Setup https server, unify http server with it, build config

---
+++ config.js
@@ -0,0 +1,27 @@
+// Create and export configuration variables
+
+// Container for all of the environments
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+    httpPort: 3000,
+    httpsPort:3001,
+    envName: 'staging'
+};
+
+// Production environment
+environments.production = {
+    httpPort: 5000,
+    httpsPort: 5001,
+    envName: 'production'
+};
+
+// Determine which enviornment was passed in as a command line argument
+const currentEnvironment = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that current environment exists, otherwise default to staging
+const environmentToExport = environments[currentEnvironment] ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
--- index.js
+++ index.js
@@ -1,35 +1,109 @@
 // Dependencies
 
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const querystring = require('querystring');
+const StringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
 
 // The server should respond to all requests with a string
 
-// Start the server, have it listen on port 3000
-
-const server = http.createServer(function(req, res) {
-  // get the URL and parse it
-  const parsedUrl = url.parse(req.url, true);
-
-  // get the path
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
-
-  // get querystring as an object
-  const queryStringObject = parsedUrl.query;
-
-  // get the HTTP method
-  const method = req.method.toLowerCase();
-
-  // send the response
-  res.end('Hello World again\n');
-
-  // log the request path
-  console.log(`Request received:\npath: ${trimmedPath}\nmethod: ${method}\nquery: ${JSON.stringify(queryStringObject)}`);
-
+// Instantiate the HTTP server
+const httpServer = http.createServer(function(req, res) {
+    unifiedServer(req, res);
 });
 
-server.listen(3000, function() {
-  console.log('the server is listening on port 3000 now');
+// Start the HTTP server
+httpServer.listen(config.httpPort, function() {
+    console.log(`the server is listening on port ${config.httpPort}`);
 });
+
+// Instantiate the HTTPs server
+const httpsServerOptions = {
+    key: fs.readFileSync('./https/key.pem'),
+    cert: fs.readFileSync('./https/cert.pem'),
+};
+
+const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP server
+httpsServer.listen(config.httpsPort, function() {
+    console.log(`the server is listening on port ${config.httpsPort}`);
+});
+
+// All the sever logic for both the http and https server
+const unifiedServer = function(req, res) {
+    // get the URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // get querystring as an object
+    const queryStringObject = parsedUrl.query;
+
+    // get the HTTP method
+    const method = req.method.toLowerCase();
+
+    // get the headers as an object
+    const headers = req.headers;
+
+    // get the payload, if there is any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // chooose the handler that this request should go to
+        const chosenHandler = router[trimmedPath] ? router[trimmedPath] : handlers.notFound;
+
+        // construct data object to send to handler
+        const data = {
+            trimmedPath,
+            queryStringObject,
+            method,
+            headers,
+            payload: buffer
+        };
+
+        // route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode = 200, payload = {}) {
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+
+            // send the response
+            res.end(JSON.stringify(payload));
+
+            // log the request path
+            console.log(`returning this response: ${statusCode} ${JSON.stringify(payload, null, 2)}`);
+        });
+    });
+};
+
+// define handlers
+const handlers = {};
+
+// Ping handler
+handlers.ping = function(data, cb) {
+    cb(200);
+};
+
+// not found handler
+handlers.notFound = function(data, cb) {
+    cb(404);
+};
+
+// define a request router
+const router ={
+    ping: handlers.ping
+};
