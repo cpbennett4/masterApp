09c5ee4326b2def84883d9d1c09b3a4c11f34896
Chucky Bennett <cpbennett4@gmail.com>
2020-04-12 16:58:12 -0700

    Add token validation to user get, put, delete methods

--- utils/handlers.js
+++ utils/handlers.js
@@ -2,28 +2,30 @@
 
 const userNameIsValid = function(name) {
     return typeof name === 'string' && name.trim().length > 0;
-}
+};
 
 const phoneIsValid = function(number) {
     return typeof number === 'string' && number.trim().length === 10;
-}
+};
 
 const passwordIsValid = function(password) {
     return typeof password === 'string' && password.trim().length > 0;
-}
+};
 
 const tosAgreementIsValid = function(tosAgreement) {
     return typeof tosAgreement === 'boolean' && tosAgreement === true;
-}
+};
 
 const idIsValid = function(id) {
     return typeof id === 'string' && id.trim().length === 20;
-}
+};
 
 const extendIsValid = function(extend) {
-    console.log('extned: ', extend);
-    console.log('extned: ', typeof extend);
     return typeof extend === 'boolean' && extend === true;
-}
+};
 
-module.exports = { userNameIsValid, phoneIsValid, passwordIsValid, tosAgreementIsValid, idIsValid, extendIsValid };
+const tokenIsValid = function(token) {
+    return typeof token === 'string';
+};
+
+module.exports = { userNameIsValid, phoneIsValid, passwordIsValid, tosAgreementIsValid, idIsValid, extendIsValid, tokenIsValid };
